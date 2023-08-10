d89104ab97ebb5859046ee1966af45d541743f1f
Rita <khakimovarita@yandex.ru>
2023-08-10 12:15:49 +0300

    Добавляю измениня по адаптивным сеткам после созвона

---
+++ source/js/script.js
@@ -0,0 +1,10 @@
+const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.nav__toggle')
+
+if (navBtn) {
+  navBtn.addEventListener('click', () => {
+    nav.classList.toggle('is-open');
+  })
+}
+
+
